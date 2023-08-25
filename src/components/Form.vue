40f59218b9920a65cd8c9e35823f21fb74cbbbe8
Kenneth Tang <chikin030611@gmail.com>
2023-08-25 18:03:26 +0800

    Bug fixed

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -94,6 +94,7 @@ export default {
       numOfPersons: "",
       date: "",
       time: "",
+      timeArray: "",
       timeRules: {
         hours: [9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
         minutes: [0, 15, 30, 45],
